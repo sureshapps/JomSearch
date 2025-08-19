5d073fca7d05c9d811956c6dfab2f6b32e3000f1
Suresh Kaleyannan <hello@suresh.app>
2025-08-19 17:29:13 +0800

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -83,7 +83,7 @@ const Index = () => {
           <div className="text-center max-w-3xl mx-auto">
             <div className="grid md:grid-cols-3 gap-8 mt-16">
               <div className="bg-card rounded-xl p-6 shadow-search hover:shadow-search-hover transition-all duration-300 hover:scale-105">
-                <div className="bg-yellow-500/10 w-12 h-12 rounded-lg flex items-center justify-center mx-auto mb-4">
+                <div className="bg-lime-500/10 w-12 h-12 rounded-lg flex items-center justify-center mx-auto mb-4">
                   <svg className="w-6 h-6 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
                   </svg>
@@ -93,7 +93,7 @@ const Index = () => {
               </div>
               
               <div className="bg-card rounded-xl p-6 shadow-search hover:shadow-search-hover transition-all duration-300 hover:scale-105">
-                <div className="bg-accent/10 w-12 h-12 rounded-lg flex items-center justify-center mx-auto mb-4">
+                <div className="bg-lime-500/10 w-12 h-12 rounded-lg flex items-center justify-center mx-auto mb-4">
                   <svg className="w-6 h-6 text-accent" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
@@ -103,7 +103,7 @@ const Index = () => {
               </div>
               
               <div className="bg-card rounded-xl p-6 shadow-search hover:shadow-search-hover transition-all duration-300 hover:scale-105">
-                <div className="bg-secondary/60 w-12 h-12 rounded-lg flex items-center justify-center mx-auto mb-4">
+                <div className="bg-lime-500/60 w-12 h-12 rounded-lg flex items-center justify-center mx-auto mb-4">
                   <svg className="w-6 h-6 text-primary" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                   </svg>
